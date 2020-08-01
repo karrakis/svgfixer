f5574ab8e98f0f8b7e34de9c5573616727ee89a1
Maman Brigitte <karrakis@gmail.com>
2020-07-31 20:10:03 -0500

    resizeable center div

--- app/javascript/packs/Home.jsx
+++ app/javascript/packs/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react"
 
 export const Home = ({data}) => {
-    return <div/>
+    return (<div className="w-screen h-screen flex items-center justify-center bg-gray-800 opacity-50">
+        <div id="boundary" className="resize overflow-auto w-32 h-32 bg-white shadow-inner"/>
+    </div>)
 }
+
+export default Home
