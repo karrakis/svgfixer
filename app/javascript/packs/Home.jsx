8023b1274f117b6aa6a0d718d4e693d3a1f38cc4
Maman Brigitte <karrakis@gmail.com>
2020-07-31 20:21:54 -0500

    boundary now reports its own width and height

--- app/javascript/packs/Home.jsx
+++ app/javascript/packs/Home.jsx
@@ -1,8 +1,15 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export const Home = ({data}) => {
+    const [boundaryXY, updateBoundaryXY] = useState()
+    useEffect(() => {
+        const el = document.getElementById("boundary")
+        updateBoundaryXY(<div className="flex flex-col"><span>{el.offsetWidth}</span><span>{el.offsetHeight}</span></div>)
+    })
     return (<div className="w-screen h-screen flex items-center justify-center bg-gray-800 opacity-50">
-        <div id="boundary" className="resize overflow-auto w-32 h-32 bg-white shadow-inner"/>
+        <div id="boundary" className="resize overflow-auto w-32 h-32 bg-white shadow-inner flex flex-col justify-center items-center">
+            <span className="my-4">{boundaryXY}</span>
+        </div>
     </div>)
 }
 
